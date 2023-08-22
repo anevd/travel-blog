58c5c991f94b01a49396bde1b603fda8b1692546
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-08-22 14:55:53 +0300

    update map

--- src/components/TravelMap/TravelMap.jsx
+++ src/components/TravelMap/TravelMap.jsx
@@ -1,25 +1,42 @@
 import React from "react";
 import styles from "./travelMap.module.css";
-import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
-import points from "../../json/points";
+import { YMaps, Map, ObjectManager } from "@pbe/react-yandex-maps";
+import { pointsOnMap } from "../../json/pointsOnMap";
 
-const TravelMap = () => (
-	<YMaps
-		query={{
-			apikey: "d0bb60d8-8b9f-4361-8dad-000a95baa8a3",
-		}}>
-		<section className={styles.map}>
-			<div className="container">
-				<h2 className={styles.map__title}>Places I want to visit next year</h2>
-				<div className={styles.map__content}>
-					<Map className={styles.map__frame} defaultState={{ center: [47.386893, 8.533977], zoom: 5 }}>
-						{points.map((point, index) => (
-							<Placemark key={index} geometry={point.coordinates} properties={{ iconCaption: point.content }} />
-						))}
-					</Map>
+function TravelMap() {
+	return (
+		<YMaps
+			query={{
+				apikey: "d0bb60d8-8b9f-4361-8dad-000a95baa8a3",
+			}}>
+			<section className={styles.map}>
+				<div className="container">
+					<h2 className={styles.map__title}>Places I want to visit next year</h2>
+					<div className={styles.map__content}>
+						<Map
+							className={styles.map__frame}
+							defaultState={{ center: [47.386893, 8.533977], zoom: 5, controls: ["zoomControl", "fullscreenControl"] }}
+							modules={["control.ZoomControl", "control.FullscreenControl"]}>
+							<ObjectManager
+								options={{
+									clusterize: true,
+									gridSize: 32,
+								}}
+								objects={{
+									openBalloonOnClick: true,
+									preset: "islands#nightStretchyIcon",
+								}}
+								clusters={{
+									preset: "islands#nightClusterIcons",
+								}}
+								defaultFeatures={pointsOnMap}
+								modules={["objectManager.addon.objectsBalloon", "objectManager.addon.objectsHint"]}
+							/>
+						</Map>
+					</div>
 				</div>
-			</div>
-		</section>
-	</YMaps>
-);
+			</section>
+		</YMaps>
+	);
+}
 export default TravelMap;
--- src/json/points.js
+++ src/json/points.js
@@ -1,32 +1,44 @@
-const points = [
+export const points = [
 	{
 		coordinates: [47.386893, 8.533977],
 		content: "Zurich",
+		image: "https://sportishka.com/uploads/posts/2022-04/1650679309_48-sportishka-com-p-tsyurikh-shveitsariya-dostoprimechatelnost-52.jpg",
+		id: 1,
 	},
 	{
 		coordinates: [41.902653, 12.495441],
 		content: "Rome",
+		image: "https://godsplanet.ru/wp-content/uploads/2022/08/colosseum-optimized.jpg",
+		id: 2,
 	},
 	{
 		coordinates: [48.137324, 11.575308],
 		content: "Munich",
+		image: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663194660_46-mykaleidoscope-ru-p-germaniya-myunkhen-dostoprimechatelnosti-49.jpg",
+		id: 3,
 	},
 	{
 		coordinates: [52.518368, 13.374546],
 		content: "Berlin",
+		image: "https://urfu.ru/fileadmin/user_upload/common_files/events/2021/02/03022021_berlin.jpg",
+		id: 4,
 	},
 	{
 		coordinates: [52.373194, 4.891469],
 		content: "Amsterdam",
+		image: "https://img5.goodfon.ru/wallpaper/nbig/b/ed/damrak-amsterdam-netherlands-damrak-amsterdam-niderlandy-dom.jpg",
+		id: 5,
 	},
 	{
 		coordinates: [51.50745, -0.127377],
 		content: "London",
+		image: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663364119_14-mykaleidoscope-ru-p-london-stolitsa-anglii-oboi-16.jpg",
+		id: 6,
 	},
 	{
 		coordinates: [53.340735, -6.253815],
 		content: "Dublin",
+		image: "https://grandgames.net/puzzle/f1200/dublinirlandiya.jpg",
+		id: 7,
 	},
 ];
-
-export default points;
